772b20f146954f867b7e005d00f612465ac7ec30
s174139 <gabriella.angela@hotmail.com>
2024-01-10 13:34:41 +0100

    try one more commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
-
+#This looks good! I made a change here :)
 
 if __name__ == '__main__':
     digits = datasets.load_digits()
